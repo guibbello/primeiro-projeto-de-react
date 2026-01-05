589b74e1358e643cfa45ef6274f3dce090b11802
Guilherme Bello <guib.bello@gmail.com>
2026-01-05 11:01:17 -0300

    Update task button layout to use flexbox

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -18,10 +18,11 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <button
